06fabf904b9e3657bb6db31943e8194c03f0fc11
Jeff Wei <jeffvwei@gmail.com>
2025-03-05 17:38:06 -0800

    feat: initial webhook and getter

---
+++ index.ts
@@ -0,0 +1,28 @@
+import express, { Request, Response } from "express"
+
+// init express app
+const app = express()
+const PORT = process.env.PORT || 3001
+app.use(express.json())
+
+// init array to store incoming payloads
+const submissions: any[] = []
+
+// create webhook to receive payloads
+app.post("/webhook", (req: Request, res: Response) => {
+  const payload = req.body
+  console.log('Received payload', JSON.stringify(payload))
+  submissions.push(payload)
+
+  res.status(200).send("Payload received")
+})
+
+// return received payloads
+app.get("/submissions", (req: Request, res: Response) => {
+  console.log('Returning submissions', submissions)
+  res.json(submissions)
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening at ${PORT}`)
+})
